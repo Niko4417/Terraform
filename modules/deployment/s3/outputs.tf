33b5029e3bcb5e06866702337bc4ef45461870be
Niko4417 <niko.vasilopoulos96@gmail.com>
2025-06-18 23:00:46 +0200

    updated outputs.tf to make github actions work

--- modules/deployment/s3/outputs.tf
+++ modules/deployment/s3/outputs.tf
@@ -2,3 +2,12 @@ output "bucket_website_url" {
   description = "Public URL of the S3 Bucket"
   value       = aws_s3_bucket.s3.website_endpoint
 }
+
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.s3.bucket_domain_name
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.s3.bucket
+}
