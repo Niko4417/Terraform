0e7d5d3e5608711e172d34675d25d5cdcf91d4bf
Niko4417 <niko.vasilopoulos96@gmail.com>
2025-06-23 22:16:28 +0200

    changed styling

--- modules/deployment/cloudfront/variables.tf
+++ modules/deployment/cloudfront/variables.tf
@@ -1,13 +1,16 @@
 variable "s3_bucket_name" {
   type = string
 }
+
 variable "s3_bucket_domain_name" {
   type = string
 }
+
 variable "tags" {
   type    = map(string)
   default = {}
 }
+
 variable "cloudfront_oai_id" {
   type        = string
 }
